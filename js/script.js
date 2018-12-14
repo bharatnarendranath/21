65d8cdb036fc088e8c44c9b844528c0da7e2af4c
Bharat Narendranath <bharatnarendranath@gmail.com>
2018-12-15 03:50:25 +0530

    completed markup and set eventlisteners

--- js/script.js
+++ js/script.js
@@ -19,14 +19,37 @@ let values = [
   "Two"
 ];
 
+let textArea = document.getElementById("text-area"),
+  newGameButton = document.getElementById("new-game-button"),
+  hitButton = document.getElementById("hit-button"),
+  stayButton = document.getElementById("stay-button");
+
+hitButton.style.display = "none";
+stayButton.style.display = "none";
+
+newGameButton.addEventListener("click", function() {
+  textArea.innerText = "Started...";
+  newGameButton.style.display = "none";
+  hitButton.style.display = "inline";
+  stayButton.style.display = "inline";
+});
+
 function createDeck() {
   let deck = [];
-  for (let suitIndex = 0; suitindex < suits.length; suitIndex++) {
+  for (let suitIndex = 0; suitIndex < suits.length; suitIndex++) {
     for (let valueIndex = 0; valueIndex < values.length; valueIndex++) {
-      deck.push(values[valueIndex] + "of" + suits[suitIndex]);
+      let card = {
+        suit: suits[suitIndex],
+        value: values[valueIndex]
+      };
+      deck.push(card);
     }
   }
   return deck;
+}
+
+function getCardString(card) {
+  return card.value + "of" + card.suit;
 }
 
 function getNextCard() {
@@ -34,14 +57,14 @@ function getNextCard() {
 }
 let deck = createDeck();
 
-for (let i = 0; i < deck.length; i++) {
-  console.log(deck[i]);
-}
+// for (let i = 0; i < deck.length; i++) {
+//   console.log(deck[i]);
+// }
 
 let playerCards = [getNextCard(), getNextCard()];
 
 console.log("Welcome to Blackjack !");
 
 console.log("You are dealt:");
-console.log(" " + playerCards[0]);
-console.log(" " + playerCards[1]);
+console.log(" " + getCardString(playerCards[0]));
+console.log(" " + getCardString(playerCards[1]));
