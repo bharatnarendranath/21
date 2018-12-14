12885057482e3dc7914fa2b47384e2f5b6fd88fc
Bharat Narendranath <bharatnarendranath@gmail.com>
2018-12-15 04:12:23 +0530

    add scores based on value

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 //Blackjack
 //
 
+//Card  variables
 let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
 let values = [
   "Ace",
@@ -19,16 +20,38 @@ let values = [
   "Two"
 ];
 
+//DOM variables
+
 let textArea = document.getElementById("text-area"),
   newGameButton = document.getElementById("new-game-button"),
   hitButton = document.getElementById("hit-button"),
   stayButton = document.getElementById("stay-button");
 
+//Game variables
+let gameStarted = false,
+  gameOver = false,
+  playerWon = false,
+  dealerCards = [],
+  playerCards = [],
+  dealerScore = 0,
+  playerScore = 0,
+  deck = [];
+
 hitButton.style.display = "none";
 stayButton.style.display = "none";
+showStatus();
 
 newGameButton.addEventListener("click", function() {
-  textArea.innerText = "Started...";
+  gameStarted = true;
+  gameOver = false;
+  playerWon = false;
+
+  deck = createDeck();
+  shuffleDeck(deck);
+  dealerCards = [getNextCard(), getNextCard()];
+  playerCards = [getNextCard(), getNextCard()];
+
+  // textArea.innerText = "Started...";
   newGameButton.style.display = "none";
   hitButton.style.display = "inline";
   stayButton.style.display = "inline";
@@ -48,8 +71,27 @@ function createDeck() {
   return deck;
 }
 
+function shuffleDeck(deck){
+  for(let i= 0; i < deck.length; i++ ){
+    let swapIndex = Math.trunc(Math.random()*deck.length);
+    let tmp = deck[swapIndex];
+    deck[swapIndex] = deck[i];
+    deck[i]=tmp;
+  }
+}
+
 function getCardString(card) {
   return card.value + "of" + card.suit;
+}
+
+function showStatus() {
+  if(!gameStarted){
+    textArea.innerText = "Welcome to Blackjack !";
+    return;
+  }
+  for(var i = 0;i<deck.length;i++){
+    textArea.innerText += '\n + getCardString(deck[i]);
+  }
 }
 
 function getNextCard() {
@@ -57,9 +99,6 @@ function getNextCard() {
 }
 let deck = createDeck();
 
-// for (let i = 0; i < deck.length; i++) {
-//   console.log(deck[i]);
-// }
 
 let playerCards = [getNextCard(), getNextCard()];
 
@@ -68,3 +107,7 @@ console.log("Welcome to Blackjack !");
 console.log("You are dealt:");
 console.log(" " + getCardString(playerCards[0]));
 console.log(" " + getCardString(playerCards[1]));
+
+// for (let i = 0; i < deck.length; i++) {
+//   console.log(deck[i]);
+// }
